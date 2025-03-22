1bc11b949e0eac283dfba8045de888b49d9a020f
Issam B. <135844893+issambhd@users.noreply.github.com>
2025-03-22 12:49:42 +0100

    Update train.py
    Challenge 0: Convert a notebook to production code

--- src/model/train.py
+++ src/model/train.py
@@ -8,11 +8,14 @@ import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
 
+# My import
+from sklearn.model_selection import train_test_split
+import mlflow
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
+    # IBE: enable autologging
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -32,9 +35,13 @@ def get_csvs_df(path):
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
-
-# TO DO: add function to split data
-
+# IBE - My code for split_data function
+def split_data(df):
+    # split the data
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
+    
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
